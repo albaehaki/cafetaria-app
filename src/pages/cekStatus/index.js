7ae77f5935aad5f7ebe0c7f7abaf2d4e1125e795
albaehaki <zackyalbaehaki@gmail.com>
2023-04-02 21:08:32 +0700

    memperbaharui

--- src/pages/cekStatus/index.js
+++ src/pages/cekStatus/index.js
@@ -55,6 +55,9 @@ export default function Example() {
                 
               </Link>
               <button
+              onClick={() =>{
+                console.log("tes")
+              }}
                 type="submit"
                 className="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
